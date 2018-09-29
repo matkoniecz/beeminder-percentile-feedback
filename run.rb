b6da985983de66156ca67d4ae08cfddd6c48775c
Mateusz Konieczny <matkoniecz@gmail.com>
2018-09-29 19:48:10 +0200

    rewrite graph generation, with splits and minor improvements

--- run.rb
+++ run.rb
@@ -9,31 +9,53 @@ def get_special_color_for_today(days_count)
   colors << 'black'
 end
 
-data = download_data()
-data.each do |entry|
-  puts entry.timestamp # last second of the affected day
-  puts entry.updated_at # time of posting/updating entry (may be on a later date)
-  puts entry.value # datapoint value
-  puts
+def get_outliers(dataset_split_into_days, percent_to_remove: 10)
+  day_counts = []
+  dataset_split_into_days.each do |dataset_for_a_day|
+    day_count = 0
+    dataset_for_a_day.each do |datapoint|
+      if datapoint.timestamp.to_date == datapoint.updated_at.to_date
+        day_count += datapoint.value
+      end
+    end
+    day_counts << day_count.to_i
+  end
+
+  day_counts.sort!
+  lower_outliers = day_counts[0..day_counts.length * percent_to_remove / 100 / 2]
+  upper_outliers = day_counts[-(day_counts.length * percent_to_remove / 100 / 2)..day_counts.length-1]
+  return lower_outliers + upper_outliers
 end
 
-split = split_into_days(data)
+def obtain_data_for_each_day()
+  data = download_data()
+  data.each do |entry|
+    puts entry.timestamp # last second of the affected day
+    puts entry.updated_at # time of posting/updating entry (may be on a later date)
+    puts entry.value # datapoint value
+    puts
+  end
 
+  return split_into_days(data)
+end
 
+def get_initialized_graph(displayed_lines)
+  size = 2000
+  g = Gruff::Line.new(size)
+  g.hide_legend = true
+  g.hide_dots = true
+  g.line_width = 1
+  g.theme = {
+    :colors => get_special_color_for_today(displayed_lines),
+    :marker_color => 'grey',
+    :font_color => 'black',
+    :background_colors => 'white'
+  }
+  return g
+end
 
-size = 2000
-g = Gruff::Line.new(size)
-g.hide_legend = true
-g.hide_dots = true
-g.theme = {
-  :colors => get_special_color_for_today(split.length),
-  :marker_color => 'grey',
-  :font_color => 'black',
-  :background_colors => 'white'
-}
-step = 15
-minutes_in_day = 24 * 60
-split.each do |dataset_for_a_day|
+def get_data_series_for_graph(dataset_for_a_day, day_date, current_time, step)
+  minutes_in_day = 24 * 60
   minute_since_day_start = step
   dataset_index = 0
   progress = [0]
@@ -43,7 +65,10 @@ split.each do |dataset_for_a_day|
       update_time = dataset_for_a_day[dataset_index].updated_at
       minutes_into_day = update_time.hour * 60 + update_time.min
       if minutes_into_day <= minute_since_day_start
-        progressed += dataset_for_a_day[dataset_index].value
+        datapoint = dataset_for_a_day[dataset_index]
+        if datapoint.timestamp.to_date == datapoint.updated_at.to_date
+          progressed += datapoint.value
+        end
         dataset_index += 1
       else
         break
@@ -52,8 +77,32 @@ split.each do |dataset_for_a_day|
     progress << progress[-1] + progressed
     minute_since_day_start += step
   end
-  puts progress.inspect
-  g.data :day, progress
+  return progress
+end
+
+
+split = obtain_data_for_each_day()
+outliers = get_outliers(split, percent_to_remove: 10)
+g = get_initialized_graph(split.length - outliers.length)
+step = 15
+# first day is guaranted to have 0 datapoint added by beeminder, so [0][0] is safe
+# -1 is done to counteract first incrementation in a loop
+# incrementation is at the beginning of loop to allow for breaks in alter part
+processed_date = split[0][0].timestamp.to_date.next_day(-1)
+now = Time.now
+split.each do |dataset_for_a_day|
+  processed_date = processed_date.next_day(1)
+  progress = get_data_series_for_graph(dataset_for_a_day, processed_date, now, step)
+
+  total_value = progress[-1].to_i
+  location_in_outliers = outliers.index(total_value)
+  if location_in_outliers != nil
+    outliers.delete_at location_in_outliers
+    puts "IGNORED AS OUTLIER (#{total_value})"
+  else
+    #puts progress.inspect
+    g.data :day, progress
+  end
 end
 
 puts "GENERATING"
