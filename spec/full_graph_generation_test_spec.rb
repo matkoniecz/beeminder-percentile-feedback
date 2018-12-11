ec4a133ff8057e18492ba204f9b0d8b5385ec9e0
Mateusz Konieczny <matkoniecz@gmail.com>
2018-12-11 17:58:42 +0100

    generate better example

--- spec/full_graph_generation_test_spec.rb
+++ spec/full_graph_generation_test_spec.rb
@@ -5,8 +5,37 @@ require 'datapoint_mock.rb'
 require_relative '../run.rb'
 
 RSpec.describe "full program behavior except calling API" do
+  def get_datapoint(date, points)
+    year = date.year
+    month = date.month
+    day = date.day
+    hour = Random.rand(24)
+    minute = Random.rand(60)
+    if Random.rand(2) == 0
+      hour = 23
+      if Random.rand(2) == 0
+        minute = Random.rand(30) + 30
+      end
+    end
+    return DatapointMock.new(DateTime.new(year, month, day), DateTime.new(year, month, day, hour, minute), points)
+  end
+  def get_data_for_a_day(date)
+    required = 50
+    returned = []
+    while required > 0
+      points = Random.rand(required) + 1
+      required -= points
+      returned << get_datapoint(date, points)
+    end
+    return returned
+  end
   it "generates graph from data" do
-    data = [DatapointMock.new(DateTime.new(2018, 12, 5), DateTime.new(2018, 12, 5, 12, 19), 1000)]
+    today = Time.now
+    data = []
+    (-100..0).each do |offset|
+      data += get_data_for_a_day(today.next_day(offset))
+      puts(data[-1])
+    end
     process_data_and_generate_graph(data)
   end
 end
